264fea908c2a2349436a491090e17e708b4d5556
ArthurBlct <124893220+ArthurBlct@users.noreply.github.com>
2024-03-25 17:17:34 +0100

    db_config removed :fire:

---
+++ index.php
@@ -0,0 +1,6 @@
+<!-- include header.php => "navbar" qui contient signup et login quand on est pas connecté -->
+<!-- sinon logout, my account et my library quand on est connecté -->
+
+<h2>Welcome to our library</h2>
+
+<?php include 'footer.php'; ?>
